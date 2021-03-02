4e33218fe69c5f4ac3b595febadf478d914da6fe
Ross Sarcona <rrsarcona@gmail.com>
2021-03-02 11:05:33 -0500

    boilerplate setup

---
+++ client/index.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from "react";
+import ReactDOM from "react-dom";
+import "./styles.scss";
+
+const App = () => {
+    return (
+        <div>tacos did i forget this already</div>
+    )
+}
+
+
+ReactDOM.render(<App/>, document.getElementById("root"))
